414b4846416e449cc11c5cc5e0b605d0b72bb971
Guillaume Munsch <guigui.munsch@gmail.com>
2016-05-09 10:35:29 -0700

    Devices finished

--- DomoThink/app/src/main/java/com/example/guillaumemunsch/domothink/activities/SearchObjectsActivity.java
+++ DomoThink/app/src/main/java/com/example/guillaumemunsch/domothink/activities/SearchObjectsActivity.java
@@ -3,13 +3,31 @@ package com.example.guillaumemunsch.domothink.activities;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.example.guillaumemunsch.domothink.R;
 import com.example.guillaumemunsch.domothink.adapter.CheckboxListAdapter;
+import com.example.guillaumemunsch.domothink.http.RestAPI;
+import com.example.guillaumemunsch.domothink.models.Device;
+import com.example.guillaumemunsch.domothink.utils.Utils;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * Created by guillaumemunsch on 03/12/15.
@@ -18,6 +36,37 @@ public class SearchObjectsActivity extends AppCompatActivity {
     TextView text = null;
     ListView list = null;
     SearchObjectsActivity context = null;
+    List<Device> foundDevices;
+
+    private void loadContent()
+    {
+        setContentView(R.layout.found_objects_activity);
+        list = (ListView)findViewById(R.id.listFoundObjects);
+
+        final ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, Utils.transform(foundDevices, "name"));
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                Utils.confirm(context, "Adding device", "Do you really want to add this device ?");
+                RequestParams params = new RequestParams();
+                params.put("device", foundDevices.get(position));
+                RestAPI.post("device", params, new JsonHttpResponseHandler() {
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        Log.d("SearchObjects: ", "Added object @ pos" + position);
+                        adapter.remove(adapter.getItem(position));
+                        adapter.notifyDataSetChanged();
+                    }
+
+                    @Override
+                    public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                        Log.d("SearchObjects: ", "Unable to add object.");
+                    }
+                });
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,28 +77,19 @@ public class SearchObjectsActivity extends AppCompatActivity {
 
         text = (TextView)findViewById(R.id.searchObjectId);
 
-        new CountDownTimer(3000, 1000) {
-
-            public void onTick(long ms) {
-                text.setText("seconds remaining: " + ms / 1000);
+        RestAPI.get("device/search", null, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                foundDevices = new Gson().fromJson(response.toString(), new TypeToken<List<Device>>() {
+                }.getType());
+                loadContent();
             }
 
-            public void onFinish() {
-                text.setText("done!");
-                setContentView(R.layout.found_objects_activity);
-                String [] objects = new String[]{
-                        "Lampe XYZ", "Temp Capture XYZ", "Autre module", "TV Smart Sony"
-                };
-                Boolean [] states = new Boolean[]{
-                        true, false, false, true
-                };
-                list = (ListView)findViewById(R.id.listFoundObjects);
-
-                list.setAdapter(new CheckboxListAdapter(context, objects, states));
-
+            @Override
+            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                Log.d("SearchObjects:", "Unable to detect objects.");
             }
-        }.start();
-
+        });
     }
 
     @Override
--- DomoThink/app/src/main/java/com/example/guillaumemunsch/domothink/models/Device.java
+++ DomoThink/app/src/main/java/com/example/guillaumemunsch/domothink/models/Device.java
@@ -12,14 +12,15 @@ public class Device implements Serializable {
     private String name;
     private String description;
     private boolean activate;
+    private boolean added;
 
-    public int getId() {
-        return id;
-    }
+    public boolean getAdded() { return added; }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+    public void setAdded(boolean added) { this.added = added; }
+
+    public int getId() { return id; }
+
+    public void setId(int id) { this.id = id; }
 
     public String getName() {
         return name;
@@ -45,11 +46,12 @@ public class Device implements Serializable {
         this.activate = activate;
     }
 
-    public Device(int id, String name, String description, boolean activate) {
+    public Device(int id, String name, String description, boolean activate, boolean added) {
 
         this.id = id;
         this.name = name;
         this.description = description;
         this.activate = activate;
+        this.added = added;
     }
 }
