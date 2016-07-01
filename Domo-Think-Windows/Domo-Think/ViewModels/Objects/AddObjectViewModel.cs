a6c36cb903ffd3b712b8603b0e52d7ead3e132cb
Filipe GOMES PEIXOTO <gomespeixoto.filipe@gmail.com>
2016-07-01 12:58:05 +0200

    Clean solution. Add DAL project. Changes on the AddObject page design.

---
+++ Domo-Think-Windows/Domo-Think/ViewModels/Objects/AddObjectViewModel.cs
@@ -0,0 +1,113 @@
+﻿using DomoThink.Model;
+using DomoThink.MVVM;
+using DomoAPI.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using DomoThink.ViewModels.Interfaces;
+
+
+/*--------------------------------------------------------
+ * AddObjectViewModel.cs
+ * 
+ * Version: 1.0
+ * Author: Filipe
+ * Created: 13/12/2015 13:53:10
+ * 
+ * Notes:
+ * -------------------------------------------------------*/
+
+namespace DomoThink.ViewModels
+{
+    public class AddObjectViewModel : ViewModelBase, ILoader
+    {
+        #region FIELDS
+        
+        private Boolean loading;
+        private Boolean display;
+
+        #endregion
+
+        #region PROPERTIES
+        
+        /// <summary>
+        /// 
+        /// </summary>
+        public ObservableCollection<ObjectModel> AvailiableObjects { get; private set; }
+
+        /// <summary>
+        /// Gets or sets the loading state.
+        /// </summary>
+        public Boolean Loading
+        {
+            get { return this.loading; }
+            set { this.NotifyPropertyChanged(ref this.loading, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the display state.
+        /// </summary>
+        public Boolean Display
+        {
+            get { return this.display; }
+            set { this.NotifyPropertyChanged(ref this.display, value); }
+        }
+
+        /// <summary>
+        /// Gets or sets the Load command.
+        /// </summary>
+        public ICommand LoadCommand { get; set; }
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+        /// <summary>
+        /// Create a new AddObjectViewModel instance.
+        /// </summary>
+        public AddObjectViewModel()
+        {
+            // Set the loading state to true (default at the begining)
+            this.SetLoadingState(true);
+
+            // Initialize the commands
+            this.LoadCommand = new Command(this.LoadAction);
+
+            // Initialize the collection
+            this.AvailiableObjects = new ObservableCollection<ObjectModel>();
+        }
+
+        #endregion
+
+        #region METHODS
+
+        /// <summary>
+        /// Set the loading state.
+        /// </summary>
+        /// <param name="state">Loading state.</param>
+        private void SetLoadingState(Boolean state)
+        {
+            this.Loading = state;
+            this.Display = !state;
+        }
+
+        #endregion
+
+        #region ACTIONS
+
+        private async void LoadAction(Object param)
+        {
+            this.SetLoadingState(true);
+
+            await Task.Delay(3000);
+
+            this.SetLoadingState(false);
+        }
+
+        #endregion
+    }
+}
--- Domo-Think-Windows/Domo-Think/Views/Objects/AddObject.xaml.cs
+++ Domo-Think-Windows/Domo-Think/Views/Objects/AddObject.xaml.cs
@@ -1,39 +1,12 @@
-﻿using DomoThink.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-// Pour plus d'informations sur le modèle d'élément Page vierge, voir la page http://go.microsoft.com/fwlink/?LinkId=234238
+﻿using DomoThink.Controls;
 
 namespace DomoThink.Views.Objects
 {
-    /// <summary>
-    /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
-    /// </summary>
-    public sealed partial class AddObject : Page
+    public sealed partial class AddObject : DomoPage
     {
         public AddObject()
         {
             this.InitializeComponent();
-            this.Loaded += AddObject_Loaded;
-        }
-
-        private void AddObject_Loaded(Object sender, RoutedEventArgs e)
-        {
-            (this.DataContext as AddObjectViewModel).SeatchDevicesCommand.Execute(null);
         }
     }
-
-    
 }
